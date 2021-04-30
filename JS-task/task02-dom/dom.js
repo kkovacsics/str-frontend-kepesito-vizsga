0eb717ed95b520572353892ed8040b63bb4a510d
kkovacsics <kalman.kovacsics@gmail.com>
2021-04-30 10:41:43 +0200

    tas02_dom ok

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,21 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const aElems = document.querySelectorAll('nav a');
+    aElems.forEach( a => {
+        if((''+a.innerText).includes('outer-link')){
+            a.target = '_blank';
+            a.innerHTML = `<strong>${a.innerHTML}</strong>`;
+        }
+    });
 
+    const nav = document.querySelector('nav');
+    nav.style.display = 'flex';
+    nav.style.flexDirection = 'column';
+    nav.style.margin = '0px auto';
+    nav.style.width = '30%';
+    nav.style.border = '1px solid blue';
+    nav.style.padding = '16px';
+
+}
+changeOuterLinks();
 
 export { changeOuterLinks };
